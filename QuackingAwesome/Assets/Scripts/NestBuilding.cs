9afd2617f3569977ad018c35359ae7cc94899e54
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-09-17 20:07:56 +0300

    added nest logic

--- QuackingAwesome/Assets/Scripts/NestBuilding.cs
+++ QuackingAwesome/Assets/Scripts/NestBuilding.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NestBuilding : MonoBehaviour
 {
     public int numberOfSticks = 0;
+    public int neededSticks;
+    public Text display;
+
+    public GameObject finishedNest;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,6 +30,14 @@ public class NestBuilding : MonoBehaviour
         {
             numberOfSticks += player.numberOfSticks;
             player.numberOfSticks = 0;
+
+            display.text = "Sticks in Nest: " + numberOfSticks + "/" + neededSticks;
+            if (numberOfSticks >= neededSticks)
+            {
+                Vector3 pos = this.gameObject.transform.position;
+                //pos = pos + new Vector3(0, 5, 0);
+                Instantiate(finishedNest, pos, Quaternion.identity);
+            }
         }
     }
 }
