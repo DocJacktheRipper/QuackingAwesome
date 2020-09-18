4297ecf5f9dc5949645ac9a77e733a214e5ca536
Jere Setälä <settis44@gmail.com>
2020-09-18 09:47:07 +0300

    Adjusted movement more
    Fixed unwanted dashes

--- QuackingAwesome/Assets/Scripts/CharacterControl.cs
+++ QuackingAwesome/Assets/Scripts/CharacterControl.cs
@@ -6,14 +6,9 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     private Rigidbody duck;
-    //IF GAME DOESNT FEEL RIGHT, CHECK THESE VALUES INSIDE UNITY
-    //unity duck drag = 1
-    //unity duck mass = 25
-    //unity duck push force = 9
-    //unity duck rotation = 3.8
 
-    public float PushForce = 9f; //duck's moving force
-    public float Rotation = 3.8f; //value how fast duck rotates
+    private float PushForce = 3f; //duck's moving force
+    private float Rotation = 3.8f; //value how fast duck rotates
     public float DuckSpeed;
 
     private Vector3 lookDir;
@@ -48,7 +43,7 @@ public class CharacterControl : MonoBehaviour
         }
 
         //get input for dash and quack(attack)
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Submit"))
         {
             dash();
         }
@@ -67,14 +62,14 @@ public class CharacterControl : MonoBehaviour
         //times the dash within 1 frame from exection
         if(Time.frameCount != dashFrame)
         {
-            if (DuckSpeed < 0.5f)
+            if (DuckSpeed < 0.3f)
             {
-                duck.AddForce(transform.forward * 150f, ForceMode.Impulse);
+                duck.AddForce(transform.forward * 100f, ForceMode.Impulse);
             }
-            else if (DuckSpeed > 0.51f && DuckSpeed < 3.25f) //antispam by determining max speed when dash is usable
+            else if (DuckSpeed > 0.31f && DuckSpeed < 1.8f) //antispam by determining max speed when dash is usable
             {
                 duck.drag = 25f;
-                duck.AddForce(transform.forward * 150f, ForceMode.Impulse);
+                duck.AddForce(transform.forward * 100f, ForceMode.Impulse);
                 duck.drag = 1f;
             }
         }
