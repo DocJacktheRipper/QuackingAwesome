8e58d8eff598ecfbd4524f7af3c497d8a56b66e8
Jere Setälä <settis44@gmail.com>
2020-09-18 09:13:31 +0300

    Adjusted movement

--- QuackingAwesome/Assets/Scripts/CharacterControl.cs
+++ QuackingAwesome/Assets/Scripts/CharacterControl.cs
@@ -6,8 +6,14 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     private Rigidbody duck;
-    public float PushForce = 15f; //duck's moving force
-    public float Rotation = 3.5f; //value how fast duck rotates
+    //IF GAME DOESNT FEEL RIGHT, CHECK THESE VALUES INSIDE UNITY
+    //unity duck drag = 1
+    //unity duck mass = 25
+    //unity duck push force = 9
+    //unity duck rotation = 3.8
+
+    public float PushForce = 9f; //duck's moving force
+    public float Rotation = 3.8f; //value how fast duck rotates
     public float DuckSpeed;
 
     private Vector3 lookDir;
@@ -48,7 +54,7 @@ public class CharacterControl : MonoBehaviour
         }
         if (Input.GetButtonDown("Fire2"))
         {
-            quack();
+            quack_placeholder.Play();
         }
     }
     void FixedUpdate()
@@ -61,22 +67,16 @@ public class CharacterControl : MonoBehaviour
         //times the dash within 1 frame from exection
         if(Time.frameCount != dashFrame)
         {
-            if (DuckSpeed < 1f)
+            if (DuckSpeed < 0.5f)
             {
-                duck.AddForce(transform.forward * 250f, ForceMode.Impulse);
-                quack_placeholder.Play();
+                duck.AddForce(transform.forward * 150f, ForceMode.Impulse);
             }
-            else if (DuckSpeed > 1.01f && DuckSpeed < 8f) //antispam by determining max speed when dash is usable
+            else if (DuckSpeed > 0.51f && DuckSpeed < 3.25f) //antispam by determining max speed when dash is usable
             {
-                duck.drag = 50f;
-                duck.AddForce(transform.forward * 400f, ForceMode.Impulse);
+                duck.drag = 25f;
+                duck.AddForce(transform.forward * 150f, ForceMode.Impulse);
                 duck.drag = 1f;
             }
         }
-    }
-
-    public void quack()
-    {
-        quack_placeholder.Play();
     }
 }
