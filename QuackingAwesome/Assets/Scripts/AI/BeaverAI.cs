1d28c409fbcad331e57a27cbcfa62fb89fd02eca
Jere Setälä <settis44@gmail.com>
2020-09-30 16:17:02 +0300

    Created AI for the beaver
    Missing stick colliding

---
+++ QuackingAwesome/Assets/Scripts/AI/BeaverAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class BeaverAI : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent beaver;
+
+    public GameObject sticks;
+
+    private float Distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sticks = GameObject.Find("CollectableSicks");
+        beaver = GetComponent<NavMeshAgent>();
+        GotoNextPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(beaver.transform.position, sticks.transform.position);
+        //Debug.Log(Distance);
+
+        if (!beaver.pathPending && beaver.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+
+        beaver.destination = points[destPoint].position;
+        destPoint = (destPoint + Random.Range(0, 18)) % points.Length;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Beaver")
+        {
+            Debug.Log("collide");
+        }
+    }
+
+    private void FetchStick()
+    {
+        
+    }
+
+   
+}
