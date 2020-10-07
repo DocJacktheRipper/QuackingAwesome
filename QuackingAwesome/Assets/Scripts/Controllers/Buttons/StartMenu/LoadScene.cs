0f069d59730e7d3b60d684c78d7e7b97f3751fea
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-10-07 19:35:09 +0300

    Added Starting Menu, added scenes to build settings

---
+++ QuackingAwesome/Assets/Scripts/Controllers/Buttons/StartMenu/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Controllers.Buttons.StartMenu
+{
+    public class LoadScene : MonoBehaviour
+    {
+        public string sceneName;
+
+        public void LoadNewScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
