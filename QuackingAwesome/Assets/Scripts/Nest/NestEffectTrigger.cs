5d24fcdeace9bab4275fa930745961b712948006
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-10-09 13:50:44 +0300

    added effect for finished nest

---
+++ QuackingAwesome/Assets/Scripts/Nest/NestEffectTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Nest
+{
+    public class NestEffectTrigger : MonoBehaviour
+    {
+        public ParticleSystem nestIsFinishedEffect;
+
+        public void NestFinishedEffect()
+        {
+            nestIsFinishedEffect.Play();
+        }
+    }
+}
