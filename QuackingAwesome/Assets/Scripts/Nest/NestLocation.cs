5d26e06ca07ec2f7d94d673825a65955890adf19
Jere Setälä <settis44@gmail.com>
2020-10-13 14:45:29 +0300

    Created an arrow to see where the nest is
    Created an arrow that appears after a certain distance when leaving area close to the nest. Arrow guides player back to the nest.

---
+++ QuackingAwesome/Assets/Scripts/Nest/NestLocation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestLocation : MonoBehaviour
+{
+    private float distance;
+    public GameObject duck;
+    public GameObject nest;
+    public GameObject arrow;
+    public Transform nestPos;
+
+    private void Start()
+    {
+        arrow.SetActive(false);
+    }
+    void Update()
+    {
+        distance = Vector3.Distance(duck.transform.position, nest.transform.position);
+        if(distance > 3f)
+        {
+            nestPosition();
+        } else
+        {
+            arrow.SetActive(false);
+        }
+    }
+
+    private void nestPosition()
+    {
+        arrow.SetActive(true);
+        arrow.transform.rotation = Quaternion.LookRotation(nestPos.position - transform.position);
+    }
+}
