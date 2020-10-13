226d2d6c61345b6cc5ad9d139e546a28bc32fa30
Jere Setälä <settis44@gmail.com>
2020-10-13 14:50:38 +0300

    -

--- QuackingAwesome/Assets/Scripts/Nest/NestLocation.cs
+++ QuackingAwesome/Assets/Scripts/Nest/NestLocation.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class NestLocation : MonoBehaviour
 {
-    private float distance;
+    public float distance;
     public GameObject duck;
     public GameObject nest;
     public GameObject arrow;
     public Transform nestPos;
 
-    private void Start()
-    {
-        arrow.SetActive(false);
-    }
     void Update()
     {
         distance = Vector3.Distance(duck.transform.position, nest.transform.position);
         if(distance > 3f)
         {
             nestPosition();
-        } else
+        } 
+        else
         {
             arrow.SetActive(false);
         }
