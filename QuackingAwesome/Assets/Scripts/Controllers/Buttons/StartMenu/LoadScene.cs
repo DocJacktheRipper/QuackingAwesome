9d6cbd16c6d11daafcfda20e4cda6e247fcbba4f
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-10-27 10:45:37 +0200

    Added Title Music

--- QuackingAwesome/Assets/Scripts/Controllers/Buttons/StartMenu/LoadScene.cs
+++ QuackingAwesome/Assets/Scripts/Controllers/Buttons/StartMenu/LoadScene.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 namespace Controllers.Buttons.StartMenu
 {
     public class LoadScene : MonoBehaviour
     {
+        public AudioMixer audioMixer;
         public string sceneName;
 
         public void LoadNewScene()
         {
+            StartFadeOut();
             SceneManager.LoadScene(sceneName);
+        }
+        
+        public void StartFadeOut()
+        {
+            StartCoroutine(FadeMusic.StartFade(audioMixer, "music_fade", 3f, 0));
         }
     }
 }
