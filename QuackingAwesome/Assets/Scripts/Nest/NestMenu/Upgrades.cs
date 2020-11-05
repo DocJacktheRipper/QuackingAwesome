11d9436c855924f424dba464b5e6ccf5732ef9df
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-11-05 21:43:33 +0200

    LevelOne bug fix and optimization

--- QuackingAwesome/Assets/Scripts/Nest/NestMenu/Upgrades.cs
+++ QuackingAwesome/Assets/Scripts/Nest/NestMenu/Upgrades.cs
@@ -1,59 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Inventory;
+﻿using Inventory;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Upgrades : MonoBehaviour
+namespace Nest.NestMenu
 {
-    public Button dashUpgrade;
-    public Button beakUpgrade;
-
-    private DucklingsInventory ducklings;
-    private DashingBehaviour dashingBehaviour;
-    private StickInventory stickInventory;
-    private int _neededAmountForDash;
-
-    public int NeededAmountForDash { get; set; }
-    public int NeededAmountForBeak { get; set; }
-
-    private void Start()
+    public class Upgrades : MonoBehaviour
     {
-        var d = GameObject.Find("Duck");
-        ducklings = d.GetComponent<DucklingsInventory>();
-        stickInventory = d.GetComponent<StickInventory>();
+        public Button dashUpgrade;
+        public Button beakUpgrade;
 
-        dashingBehaviour = d.GetComponent<DashingBehaviour>();
-    }
+        private DucklingsInventory ducklings;
+        private DashingBehaviour dashingBehaviour;
+        private StickInventory stickInventory;
+        private int _neededAmountForDash;
 
-    private void Update()
-    {
-        // Dash cooldown
-        //dashUpgrade.enabled = ducklings.DucklingCount >= NeededAmountForDash;
-        dashUpgrade.interactable = ducklings.DucklingCount >= NeededAmountForDash;
+        public int NeededAmountForDash { get; set; }
+        public int NeededAmountForBeak { get; set; }
 
-        // Beak capacity
-        beakUpgrade.interactable = ducklings.DucklingCount >= NeededAmountForBeak;
-    }
+        private void Start()
+        {
+            var d = GameObject.Find("Duck");
+            ducklings = d.GetComponent<DucklingsInventory>();
+            stickInventory = d.GetComponent<StickInventory>();
+
+            dashingBehaviour = d.GetComponent<DashingBehaviour>();
+        }
+
+        private void Update()
+        {
+            // Dash cooldown
+            //dashUpgrade.enabled = ducklings.DucklingCount >= NeededAmountForDash;
+            dashUpgrade.interactable = ducklings.DucklingCount >= NeededAmountForDash;
+
+            // Beak capacity
+            beakUpgrade.interactable = ducklings.DucklingCount >= NeededAmountForBeak;
+        }
 
 
-    /******** Button functions ********/
-    public void UpgradeDashCooldown(float amount)
-    {
-        dashingBehaviour.cooldown -= amount;
-        ducklings.RemoveDucklings(NeededAmountForDash);
+        /******** Button functions ********/
+        public void UpgradeDashCooldown(float amount)
+        {
+            dashingBehaviour.cooldown -= amount;
+            ducklings.RemoveDucklings(NeededAmountForDash);
 
-        NeededAmountForDash += 1;
-        var text = dashUpgrade.transform.Find("RequirementText").GetComponent<Text>();
-        text.text = "Cost: " + NeededAmountForDash + " ducklings";
-    }
+            NeededAmountForDash += 1;
+            var text = dashUpgrade.transform.Find("CostText").GetComponent<Text>();
+            text.text = NeededAmountForDash.ToString();
+        }
 
-    public void UpgradeCarryCapacity(int amount)
-    {
-        stickInventory.maxCapacityOfSticks += amount;
-        Debug.Log("New capacity: " + stickInventory.maxCapacityOfSticks);
-        ducklings.RemoveDucklings(NeededAmountForBeak);
+        public void UpgradeCarryCapacity(int amount)
+        {
+            stickInventory.maxCapacityOfSticks += amount;
+            Debug.Log("New capacity: " + stickInventory.maxCapacityOfSticks);
+            ducklings.RemoveDucklings(NeededAmountForBeak);
 
-        NeededAmountForBeak += 1;
+            NeededAmountForBeak += 1;
+            var text = beakUpgrade.transform.Find("CostText").GetComponent<Text>();
+            text.text = NeededAmountForBeak.ToString();
+        }
     }
 }
