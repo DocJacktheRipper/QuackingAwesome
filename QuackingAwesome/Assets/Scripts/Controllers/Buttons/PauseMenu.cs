c01bae5d8ea5d75dfa7bbd08a661fa143656f242
DocJacktheRipper <48795292+DocJacktheRipper@users.noreply.github.com>
2020-11-20 15:02:02 +0200

    sound fixed

--- QuackingAwesome/Assets/Scripts/Controllers/Buttons/PauseMenu.cs
+++ QuackingAwesome/Assets/Scripts/Controllers/Buttons/PauseMenu.cs
@@ -58,7 +58,7 @@ namespace Controllers.Buttons
             EnableOtherUI(true);
             pauseMenuUi.SetActive(false);
             Time.timeScale = 1f;
-            audio.SetFloat("sound_volume", 0);
+            audio.SetFloat("sound_volume", 1);
         
             GameIsPaused = false;
         }
